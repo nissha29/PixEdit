19e2c741772a600d27c348df61de46dd521e63df
nissha29 <nishakashyap2907@gmail.com>
2025-09-16 19:26:07 +0530

    fixed fonts error

---
+++ src/lib/fonts.ts
@@ -0,0 +1,49 @@
+import { Lato, Montserrat, Open_Sans, Oswald, Playfair_Display, Poppins, Quattrocento, Roboto } from "next/font/google";
+
+export const quattrocento = Quattrocento({
+  weight: ['400', '700'],
+  variable: "--font-quattrocento",
+  subsets: ['latin'],
+});
+
+export const roboto = Roboto({
+  weight: ["400", "700"],
+  variable: "--font-roboto",
+  subsets: ["latin"],
+});
+
+export const openSans = Open_Sans({
+  weight: ["400", "700"],
+  variable: "--font-open-sans",
+  subsets: ["latin"],
+});
+
+export const lato = Lato({
+  weight: ["400", "700"],
+  variable: "--font-lato",
+  subsets: ["latin"],
+});
+
+export const montserrat = Montserrat({
+  weight: ["400", "700"],
+  variable: "--font-montserrat",
+  subsets: ["latin"],
+});
+
+export const poppins = Poppins({
+  weight: ["400", "700"],
+  variable: "--font-poppins",
+  subsets: ["latin"],
+});
+
+export const playfairDisplay = Playfair_Display({
+  weight: ["400", "700"],
+  variable: "--font-playfair-display",
+  subsets: ["latin"],
+});
+
+export const oswald = Oswald({
+  weight: ["400", "700"],
+  variable: "--font-oswald",
+  subsets: ["latin"],
+});
